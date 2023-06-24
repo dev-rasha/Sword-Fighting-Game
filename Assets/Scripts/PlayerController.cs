2dd569fbdc43f7eadc63f01ec11bede32f288d51
R13TechNewbie <ramadhanhidayat13@gmail.com>
2023-06-24 12:01:06 +0700

    akurasi waktu animasi skill

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -258,12 +258,26 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator IncrementCooldownBar()
     {
-        float incrementAmount = 0.02f;
-        float targetFillAmount = 1f;    
+        // float incrementAmount = 0.02f;
+        // float targetFillAmount = 1f;    
 
+        // while (CooldownBar1.fillAmount < targetFillAmount)
+        // {
+        //     yield return new WaitForSeconds(0.1f);
+        //     CooldownBar1.fillAmount += incrementAmount;
+        // }
+
+        // CooldownBar1.fillAmount = targetFillAmount;
+
+
+        float targetFillAmount = 1f;
+        float totalTime = 5f;
+        float incrementAmount = targetFillAmount / totalTime;
+        float waitTime = totalTime / (targetFillAmount / incrementAmount);
+        
         while (CooldownBar1.fillAmount < targetFillAmount)
         {
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(waitTime);
             CooldownBar1.fillAmount += incrementAmount;
         }
 
